92d2ef11cf861c0082a5d4428c96ab51aec7c7ed
Max Bebök <beboek.max@gmail.com>
2023-11-25 14:09:17 +0100

    fix: error out on shader compile error

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
-use std::process::Command;
+use std::process::{Command};
+use std::str;
 
 fn main() {
-  Command::new("glslc")
+  let res = Command::new("glslc")
     .args(&["-O", "--target-env=vulkan1.2", "-fshader-stage=compute", "src/shader/shader.glsl", "-o", "src/shader/shader.spv"])
-    .output()
-    .expect("Failed to cmpiler shader.glsl to shader.spv");
+    .output().expect("Failed to compile shader");
+
+  println!("Shader: {}", str::from_utf8(&res.stderr).unwrap());
+
+  std::process::exit(res.status.code().unwrap_or(1));
 }
